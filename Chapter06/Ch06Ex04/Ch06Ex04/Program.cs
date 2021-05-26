2cfeec676d9fd7ef0c588dfd9c2c7253d25114c1
salem aymen <65363833+acemen@users.noreply.github.com>
2021-05-26 18:24:27 -0400

    Add files via upload
    2021/05/26

---
+++ Chapter06/Ch06Ex04/Ch06Ex04/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch06Ex04
+{
+    class Program
+    {
+        static void Write()
+        {
+            Console.WriteLine("myString = {0}", myString);
+        }
+
+        static void Main(string[] args)
+        {
+            string myString = "String defined in Main()";
+            Write();
+            Console.ReadKey();
+        }
+    }
+}
